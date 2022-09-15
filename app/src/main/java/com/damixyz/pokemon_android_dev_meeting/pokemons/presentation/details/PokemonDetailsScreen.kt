7b0f35def4a518371e013fcfe7fd34db1c850fad
dami <damixyz@outlook.com>
2022-09-15 19:22:55 +0100

    refactor

--- app/src/main/java/com/damixyz/pokemon_android_dev_meeting/pokemons/presentation/details/PokemonDetailsScreen.kt
+++ app/src/main/java/com/damixyz/pokemon_android_dev_meeting/pokemons/presentation/details/PokemonDetailsScreen.kt
@@ -1,9 +1,12 @@
 package com.damixyz.pokemon_android_dev_meeting.pokemons.presentation.details
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun PokemonDetailsScreen() {
-    Text(text = "Hello world from Details Screen ✅")
+    Column {
+        Text(text = "Hello world from Details Screen ✅")
+    }
 }
